14ac406e3a66a0c4bd44b6e3f3ce000af7ebae18
Robin F. Pronk <robin.pronk@nedap.com>
2022-02-15 15:04:46 +0100

    debug

--- prometheus_process_exporter/rootfs/etc/cont-init.d/process_exporter.sh
+++ prometheus_process_exporter/rootfs/etc/cont-init.d/process_exporter.sh
@@ -9,9 +9,6 @@ echo "${SUPERVISOR_TOKEN}" > '/run/home-assistant.token'
 # Even if the user isn't using these options, we're creating the web config file
 # This will allow us to append to the web config file as needed (based on variables)
 # process-exporter will run with a blank web config file in the meantime
-web_config_file=/etc/prometheus_process-exporter_web.yml
+web_config_file=/etc/prometheus_process-exporter.yml
 chmod 740 $web_config_file
 chown root:prometheus $web_config_file
-
-# Poor man's debugger: check permissions on web config file
-#ls -la $web_config_file
